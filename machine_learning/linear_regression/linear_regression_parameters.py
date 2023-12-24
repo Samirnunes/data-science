2d800a110d9e0c240a0dc67c1876df17e49cdc81
Samir Nunes da Silva <samir.silva12342@gmail.com>
2023-12-24 11:07:10 -0300

    organized imports

--- machine_learning/linear_regression/linear_regression_parameters.py
+++ machine_learning/linear_regression/linear_regression_parameters.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append('../')
 from models.parameters import Parameters
 
 class LinearRegressionParameters(Parameters):
--- machine_learning/linear_regression/linear_regressor.py
+++ machine_learning/linear_regression/linear_regressor.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append('../')
 import numpy as np
 from copy import deepcopy
 from models.supervised_model import *
