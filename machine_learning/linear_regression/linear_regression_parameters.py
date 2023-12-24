06cc5783bd90deb99eb5c028c051ad988b50f749
Samir Nunes da Silva <samir.silva12342@gmail.com>
2023-12-24 10:39:43 -0300

    Parameters to abstract class

---
+++ machine_learning/linear_regression/linear_regression_parameters.py
@@ -0,0 +1,13 @@
+from parameters import Parameters
+
+class LinearRegressionParameters(Parameters):
+    def __init__(self):
+        self.initial_weights = []
+        self.initial_bias = 0
+        self.epochs = 100
+        self.batch_size = 10
+        self.alpha = 0.1
+        self.lambda_reg = 0.1
+        self.random_state = 0
+        self.ws = []
+        self.b = 0
--- machine_learning/parameters.py
+++ machine_learning/parameters.py
@@ -1,12 +1,9 @@
-class Parameters:
+from abc import ABC, abstractmethod
+
+class Parameters(ABC):
+    @abstractmethod
     def __init__(self):
-        self.initial_weights = []
-        self.initial_bias = 0
-        self.epochs = 100
-        self.batch_size = 10
-        self.alpha = 0.1
-        self.lambda_reg = 0.1
-        self.random_state = 0
+        pass
 
     def __str__(self):
         string = ""
