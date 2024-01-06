832bb3e2f6ddd8e2247c1e3c9a39df540dfa4796
Samir Nunes da Silva <samir.silva12342@gmail.com>
2024-01-06 16:24:16 -0300

    added scaling technique

---
+++ machine_learning/machine_learning_techniques/scaling/scaler.py
@@ -0,0 +1,31 @@
+import sys
+sys.path.append('../')
+import pandas as pd
+from utils.churn_modelling_utils import num_cat_cols
+from sklearn.preprocessing import (
+    MinMaxScaler,
+    RobustScaler,
+    StandardScaler,
+)
+
+class Scaler:
+    def __init__(self, scale_method: str):
+        self.scale_method = scale_method
+    
+    def scale(self, X_train, X_test):
+        num_cols, _ = num_cat_cols(X_train)
+        X_train_num = X_train[num_cols].copy()
+        X_test_num = X_test[num_cols].copy()
+        scaler = self.__select_scaler()
+        X_train[num_cols] = pd.DataFrame(scaler.fit_transform(X_train_num), columns = X_train_num.columns)
+        X_test[num_cols] = pd.DataFrame(scaler.transform(X_test_num), columns = X_train_num.columns)
+        return X_train, X_test
+        
+    def __select_scaler(self):
+        if(self.scale_method.lower() == "minmax"):
+            return MinMaxScaler()
+        if(self.scale_method.lower() == "robust"):
+            return RobustScaler()
+        if(self.scale_method.lower() == "standard"):
+            return StandardScaler()
+        return StandardScaler()
