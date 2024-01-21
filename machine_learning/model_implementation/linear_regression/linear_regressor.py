1e833f8ab2f8b8ad45ebcd743100240ef672506b
Samir Nunes da Silva <samir.silva12342@gmail.com>
2024-01-21 20:17:15 -0300

    add stochastic component to linear regressor

--- machine_learning/model_implementation/linear_regression/linear_regressor.py
+++ machine_learning/model_implementation/linear_regression/linear_regressor.py
@@ -41,6 +41,9 @@ class LinearRegressor(SupervisedModel):
     def _sgd_update(self, X_train, y_train):
         total_rows = len(y_train)
         batch_rows = 0
+        random_indices = np.random.permutation(len(y_train))
+        X_train_shuffled = X_train.iloc[random_indices].reset_index(drop=True).copy()
+        y_train_shuffled = y_train.iloc[random_indices].reset_index(drop=True).copy()
         while batch_rows != total_rows:
             initial_index = batch_rows
             if total_rows - batch_rows > self._parameters.batch_size:
@@ -49,8 +52,8 @@ class LinearRegressor(SupervisedModel):
             else:
                 final_index = total_rows
                 batch_rows = total_rows
-            X_batch = X_train.iloc[initial_index:final_index].copy()
-            y_batch = y_train.iloc[initial_index:final_index].copy()
+            X_batch = X_train_shuffled.iloc[initial_index:final_index].copy()
+            y_batch = y_train_shuffled.iloc[initial_index:final_index].copy()
             correction_constant = self._parameters.batch_size / (final_index - initial_index)
             self._batch_update(X_batch, y_batch, self._parameters.batch_size, correction_constant)
 
